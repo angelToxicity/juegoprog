ff21ae0b70303850f04a3345d9d84f9c79f55c9d
Angel R <angel.rodriguez.m@inpsasel.gob.ve>
2018-04-02 15:59:45 -0400

    Subiendo proyecto de programación

---
+++ hol.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+	cout << "Bienvenido a Decodificado" << endl;
+	string nombre1;
+	cin >> mensaje;
+	cout << mensaje << endl;
+	/*do
+	{
+ 	} while (intento == 'n');*/
+
+	return 0;
+}
