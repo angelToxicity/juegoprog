5f032ef6b1da1694edc0798ba8026c27dd3c5b84
Angel R <angel.rodriguez.m@inpsasel.gob.ve>
2018-04-03 16:31:52 -0400

    Subiendo Avances del juego, casi total el progreso

--- hola.cpp
+++ hola.cpp
@@ -8,10 +8,8 @@ int main(int argc, char const *argv[])
 {
 	string nombre1, nombre2;
 	unsigned char next;
-	char intento;
-	int numRobot = 1, i, j, ocupado = 1;
+	int numRobot = 1, ocupado = 1, intento = 6, i, j, h, x, y;
 	int tablero[6][6];
-
 
 	cout << "Bienvenido a Decodificado\n" << endl;
 	cout << "\nReglas del juego: \n 1.El juego se desenvuelve en un tablero, el cual es una cuadrícula de 6x6\n 2.Se solicitarán las coordenadas de cada uno de los robots\n 3.Una vez posicionados los 6 robots en el tablero, empieza el juego.\n 4.El jugador indicará una coordenada y si ese cuadro está ocupado por un robot, este será decodificado y esa casilla es libre.\n 5.El juego termina cuando se hayan decodificado todos los robots o se hayan agotado las oportunidades." << endl;
@@ -19,13 +17,16 @@ int main(int argc, char const *argv[])
 	next = getchar();
 
 	if(next) {
+
 		cout << "Introduzca el nombre del 1er jugador:"  << endl;
 		cin >> nombre1;
 		cout << "Introduzca el nombre del 2do jugador:"  << endl;
 		cin >> nombre2;
 
+		//Inicializo matriz: tablero
 		i = 0;
 		j = 0;
+		h = 0;
 
 		while(i < 6) {
 		    while(j < 6) {
@@ -36,51 +37,181 @@ int main(int argc, char const *argv[])
 		    i++;
 		}
 
+		//Lleno la matriz: tablero
 		i = 0;
 		j = 0;
 
 		while(numRobot < 7) {
-
-			cout << "\nIngrese las coordenadas en X del robot n°" << numRobot << endl;
-			cin >> j;
-			if(j >= 5 || j < 0) {
-				while(j >= 5 || j < 0) {
-					cout << "Esta posición no es válida. Intente otra posición\n" << endl;
-					cout << "\nIngrese las coordenadas en X del robot n°" << numRobot << endl;
-					cin >> j;
-				}
-			}
-			cout << "\nIngrese las coordenadas en Y del robot n°" << numRobot << endl;
+			cout << "\nIngrese la coordenada en X del robot n°" << numRobot << endl;
 			cin >> i;
 			if(i < 0 || i > 5) {
 				while(i < 0 || i > 5) {
 					cout << "Esta posición no es válida. Intente otra posición" << endl;
-					cout << "\nIngrese las coordenadas en Y del robot n°" << numRobot << endl;
+					cout << "\nIngrese la coordenada en X del robot n°" << numRobot << endl;
 					cin >> i;
 				}
 			}
-			tablero[i][j] = ocupado;
-			tablero[i][j+1] = ocupado;
-			numRobot++;
-			ocupado++;
+			cout << "\nIngrese la coordenada en Y del robot n°" << numRobot << endl;
+			cin >> j;
+			if(j > 4 || j < 0) {
+				while(j > 4 || j < 0) {
+					cout << "Esta posición no es válida. Intente otra posición\n" << endl;
+					cout << "\nIngrese la coordenada en Y del robot n°" << numRobot << endl;
+					cin >> j;
+				}
+			}
+			for (int t = 0; t < 6; ++t)
+			{
+				if(tablero[i][t] != 0) {
+					h++;
+				}
+			}
+			if(h > 0) {
+				cout << endl;
+				cout << "---------------------------------------------" << endl;
+				cout << "Esta fila está llena. Intente otra posición\n" << endl;
+				cout << "---------------------------------------------" << endl;
+				h = 0;
+			}
+			else {
+				tablero[i][j] = ocupado;
+				tablero[i][j+1] = ocupado;
+				numRobot++;
+				ocupado++;
+			}
 		}
+	}
 
-	/*	do
-		{
-			while(numRobot < 7) {
-				cout << "\nIngrese las coordenadas en X del robot" << numRobot << endl;
-				cout << "\nIngrese las coordenadas en Y del robot" << numRobot << endl;
-			}
-	 	} while (intento == 'n');*/
+	//Jugador 2.
+	x = 0;
+	y = 0;
 
-		//Función para mostrar matriz
-		for (i=0;i<6;i++)
-		{	
-			for (j=0;j<6;j++)
+	while(intento > 0) {
+
+		for (i = 0; i < 6; i++)
 			{	
-				printf("[%d]",tablero[i][j]);							
+				for (j = 0; j < 6; j++)
+				{	
+					printf("[%d]",tablero[i][j]);
+				}
+				printf("\n");
 			}
-			printf("\n");
+		
+		cout << "\n\nTiene " << intento << " intentos." << endl;
+		cout << "\nIngrese la coordenada en X" << endl;
+		cin >> x;
+		if(x < 0 || x > 5) {
+			while(x < 0 || x > 5) {
+				cout << "Esta posición no es válida. Intente otra posición" << endl;
+				cout << "\nIngrese la coordenada en X" << endl;
+				cin >> x;
+			}
+		}
+		cout << "\nIngrese la coordenada en Y" << endl;
+		cin >> y;
+		if(y > 4 || y < 0) {
+			while(y > 4 || y < 0) {
+				cout << "Esta posición no es válida. Intente otra posición\n" << endl;
+				cout << "\nIngrese la coordenada en Y" << endl;
+				cin >> y;
+			}
+		}
+		if (tablero[x][y] != 0)
+		{
+			if(tablero[x][y] == tablero[x][y+1]) {
+				tablero[x][y] = 0;
+				tablero[x][y+1] = 0;	
+				cout << "Decodificó un robot. Tiene otra oportunidad.\n" << endl;
+				for (int u = 0; u < 6; ++u)
+				{
+						for (int m = 0; m < 6; ++m)
+						{
+							if(tablero[u][m] == 0){
+								cout << "no queda ningun robot" << endl;
+								break;
+							}
+						}
+				}
+				
+			}
+			else {
+				tablero[x][y] = 0;
+				tablero[x][y-1] = 0;
+				cout << "Decodificó un robot. Tiene otra oportunidad.\n" << endl;
+				for (int u = 0; u < 6; ++u)
+				{
+					if(tablero[u] == 0){
+						for (int m = 0; m < 6; ++m)
+						{
+							if(tablero[u][m] == 0){
+								cout << "no queda ningun robot" << endl;
+							}
+						}
+					}
+				}
+			}
+		}else{
+			intento--;
+			cout << "No Decodificó ningún robot. Ha perdido una oportunidad.\n" << endl;
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+	if (intento == 0)
+	{
+		cout << endl;
+		cout << "---------------------------------------------" << endl;
+		cout << nombre2 << " ha perdido. Se ha quedado sin intentos\n" << endl;
+		cout << "---------------------------------------------" << endl;
+		cout << endl;
+		cout << endl;
+	}
+	else{
+		h = 0;
+
+		for (int w = 0; w < 6; ++w)
+		{
+			for (int l = 0; l < 6; ++l)
+			{
+				if(tablero[w][l] != 0) {
+					h++;
+				}
+			}
+		}
+		if(h > 0) {
+			cout << endl;
+			cout << "---------------------------------------------" << endl;
+			cout << nombre2 << " ha perdido. Aún quedan robots sin decodificar\n" << endl;
+			cout << "---------------------------------------------" << endl;
+			cout << endl;
+			cout << endl;
+			//Blucle para mostrar matriz
+			for (i = 0; i < 6; i++)
+			{	
+				for (j = 0; j < 6; j++)
+				{	
+					printf("[%d]",tablero[i][j]);							
+				}
+				printf("\n");
+			}
+		}
+		else{
+			cout << endl;
+			cout << "---------------------------------------------" << endl;
+			cout << nombre2 << " ha ganado.\n" << endl;
+			cout << "---------------------------------------------" << endl;
+
 		}
 	}
 
