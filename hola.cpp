0371a3d3bd9e35dc52780ca6632ebe8e836c9b93
angel <klju784@gmail.com>
2018-04-03 20:42:11 -0400

    Subiendo juego finalizado. Falta opción de volver a jugar

--- hola.cpp
+++ hola.cpp
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[])
 	string nombre1, nombre2;
 	unsigned char next;
 	int numRobot = 1, ocupado = 1, intento = 6, i, j, h, x, y;
-	int tablero[6][6];
+	int tablero[6][6], matriz[6][6];
 
 	cout << "Bienvenido a Decodificado\n" << endl;
 	cout << "\nReglas del juego: \n 1.El juego se desenvuelve en un tablero, el cual es una cuadrícula de 6x6\n 2.Se solicitarán las coordenadas de cada uno de los robots\n 3.Una vez posicionados los 6 robots en el tablero, empieza el juego.\n 4.El jugador indicará una coordenada y si ese cuadro está ocupado por un robot, este será decodificado y esa casilla es libre.\n 5.El juego termina cuando se hayan decodificado todos los robots o se hayan agotado las oportunidades." << endl;
@@ -27,7 +27,6 @@ int main(int argc, char const *argv[])
 		i = 0;
 		j = 0;
 		h = 0;
-
 		while(i < 6) {
 		    while(j < 6) {
 		        tablero[i][j] = 0;
@@ -37,9 +36,23 @@ int main(int argc, char const *argv[])
 		    i++;
 		}
 
+		i = 0;
+		j = 0;
+		h = 0;
+
+		while(i < 6) {
+		    while(j < 6) {
+		        matriz[i][j] = 0;
+		        j++;
+		    }
+		    j = 0;
+		    i++;
+		}
+
 		//Lleno la matriz: tablero
 		i = 0;
 		j = 0;
+
 
 		while(numRobot < 7) {
 			cout << "\nIngrese la coordenada en X del robot n°" << numRobot << endl;
@@ -75,7 +88,9 @@ int main(int argc, char const *argv[])
 			}
 			else {
 				tablero[i][j] = ocupado;
+				matriz[i][j] = ocupado;
 				tablero[i][j+1] = ocupado;
+				matriz[i][j+1] = ocupado;
 				numRobot++;
 				ocupado++;
 			}
@@ -87,16 +102,8 @@ int main(int argc, char const *argv[])
 	y = 0;
 
 	while(intento > 0) {
-
-		for (i = 0; i < 6; i++)
-			{	
-				for (j = 0; j < 6; j++)
-				{	
-					printf("[%d]",tablero[i][j]);
-				}
-				printf("\n");
-			}
-		
+	
+		cout << "\nListo, " << nombre2 << ". Ingrese las coordenadas solicitadas para decodificar los robots" << endl;
 		cout << "\n\nTiene " << intento << " intentos." << endl;
 		cout << "\nIngrese la coordenada en X" << endl;
 		cin >> x;
@@ -120,34 +127,53 @@ int main(int argc, char const *argv[])
 		{
 			if(tablero[x][y] == tablero[x][y+1]) {
 				tablero[x][y] = 0;
-				tablero[x][y+1] = 0;	
-				cout << "Decodificó un robot. Tiene otra oportunidad.\n" << endl;
-				for (int u = 0; u < 6; ++u)
+				tablero[x][y+1] = 0;
+
+				h = 0;
+				for (int w = 0; w < 6; ++w)
 				{
-						for (int m = 0; m < 6; ++m)
-						{
-							if(tablero[u][m] == 0){
-								cout << "no queda ningun robot" << endl;
-								break;
-							}
+					for (int l = 0; l < 6; ++l)
+					{
+						if(tablero[w][l] == 0) {
+							h++;
 						}
+					}
 				}
-				
+				if(h == 36) {
+					cout << endl;
+					cout << "---------------------------------------------" << endl;
+					cout << nombre2 << " ha ganado.\n" << endl;
+					cout << "---------------------------------------------" << endl;
+					break;
+				}
+				else{
+					cout << "Decodificó un robot. Tiene otra oportunidad.\n" << endl;
+				}
+
 			}
 			else {
 				tablero[x][y] = 0;
 				tablero[x][y-1] = 0;
-				cout << "Decodificó un robot. Tiene otra oportunidad.\n" << endl;
-				for (int u = 0; u < 6; ++u)
+
+				h = 0;
+				for (int w = 0; w < 6; ++w)
 				{
-					if(tablero[u] == 0){
-						for (int m = 0; m < 6; ++m)
-						{
-							if(tablero[u][m] == 0){
-								cout << "no queda ningun robot" << endl;
-							}
+					for (int l = 0; l < 6; ++l)
+					{
+						if(tablero[w][l] == 0) {
+							h++;
 						}
 					}
+				}
+				if(h == 36) {
+					cout << endl;
+					cout << "---------------------------------------------" << endl;
+					cout << nombre2 << " ha ganado.\n" << endl;
+					cout << "---------------------------------------------" << endl;
+					break;
+				}
+				else{
+					cout << "Decodificó un robot. Tiene otra oportunidad.\n" << endl;
 				}
 			}
 		}else{
@@ -155,18 +181,6 @@ int main(int argc, char const *argv[])
 			cout << "No Decodificó ningún robot. Ha perdido una oportunidad.\n" << endl;
 		}
 	}
-
-
-
-
-
-
-
-
-
-
-
-
 
 	if (intento == 0)
 	{
@@ -176,44 +190,38 @@ int main(int argc, char const *argv[])
 		cout << "---------------------------------------------" << endl;
 		cout << endl;
 		cout << endl;
-	}
-	else{
-		h = 0;
-
-		for (int w = 0; w < 6; ++w)
-		{
-			for (int l = 0; l < 6; ++l)
-			{
-				if(tablero[w][l] != 0) {
-					h++;
-				}
-			}
-		}
-		if(h > 0) {
-			cout << endl;
-			cout << "---------------------------------------------" << endl;
-			cout << nombre2 << " ha perdido. Aún quedan robots sin decodificar\n" << endl;
-			cout << "---------------------------------------------" << endl;
-			cout << endl;
-			cout << endl;
-			//Blucle para mostrar matriz
-			for (i = 0; i < 6; i++)
+		//Blucle para mostrar matriz
+		for (i = 0; i < 6; i++)
+		{	
+			for (j = 0; j < 6; j++)
 			{	
-				for (j = 0; j < 6; j++)
-				{	
-					printf("[%d]",tablero[i][j]);							
-				}
-				printf("\n");
+				printf("[%d]",matriz[i][j]);							
 			}
-		}
-		else{
-			cout << endl;
-			cout << "---------------------------------------------" << endl;
-			cout << nombre2 << " ha ganado.\n" << endl;
-			cout << "---------------------------------------------" << endl;
-
+			printf("\n");
 		}
 	}
+	cout << "¿Desea volver a jugar?\n" << endl;
+	cout << "Introduzca s para continuar el juego." << endl;
+	cout << "Introduzca n para finalizar el juego." << endl;
+	cin >> vuelta;
+/*	else{
+	cout << endl;
+	cout << "---------------------------------------------" << endl;
+	cout << nombre2 << " ha perdido. Aún quedan robots sin decodificar\n" << endl;
+	cout << "---------------------------------------------" << endl;
+	cout << endl;
+	cout << endl;
+	//Blucle para mostrar matriz
+	for (i = 0; i < 6; i++)
+	{	
+		for (j = 0; j < 6; j++)
+		{	
+			printf("[%d]",matriz[i][j]);							
+		}
+		printf("\n");
+	}
+
+}*/
 
 	return 0;
 }
